0a76b6e83b44c535360fc5b27fb9d18e5df77cd8
PolinaTimofeeva <polleyw@mail.ru>
2022-05-23 11:05:17 +0300

    main settings

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='header'>
+        <div className='container'>
+          <div className='header__body'>
+            <div className='header__logo'>
+              <img src={logo} alt='Логотип компании' />
+              
+              <div className='header__buttons'>
+                <button className='btn__white'>Версия для слабовидящих</button>
+                <button className='btn__white'>Мой профиль</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
